5423e8baa3011c192f03a5806b505f096fc2e2c7
SimiF <SimiF@users.noreply.github.com>
2018-04-18 14:03:14 -0700

    These are updated, but not finalized.

--- CST126_2/stock.cpp
+++ CST126_2/stock.cpp
@@ -2,4 +2,18 @@
 // stock.cpp
 //--------------------------------------------------------------------
 #include "stdafx.h"
+#include "stock.h"
+#include <iostream>
 
+#include <string>
+
+void Initialize_Stock(std::string& symbol, std::string& name_with_quotes, double& price, double& earnings)
+{
+	Stock_Info I;
+	I.Ticker = symbol;
+	I.Name = name_with_quotes;
+	I.Price = price;
+	I.Earnings = earnings;
+
+	std::cout << I.Ticker << I.Name << I.Price << I.Earnings << std::endl;
+}
--- CST126_2/stock.h
+++ CST126_2/stock.h
@@ -2,3 +2,13 @@
 // stock.h
 //     
 //--------------------------------------------------------------------
+#include <string>
+
+struct Stock_Info
+{
+	std::string Ticker;
+	std::string Name;
+	double Price;
+	double Earnings;
+	double PE_Ratio;
+};
