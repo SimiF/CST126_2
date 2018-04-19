632e45eac7af3b8b0b894a3cdb09d439e64d6895
SimiF <SimiF@users.noreply.github.com>
2018-04-19 07:44:12 -0700

    These are updated but not ready for review.

--- CST126_2/stock.cpp
+++ CST126_2/stock.cpp
@@ -4,16 +4,20 @@
 #include "stdafx.h"
 #include "stock.h"
 #include <iostream>
-
 #include <string>
+#include <iomanip>
+using namespace std;
 
-void Initialize_Stock(std::string& symbol, std::string& name_with_quotes, double& price, double& earnings)
+Stock_Info::Stock_Info(string & symbol, string & name_with_quotes, double & price, double & earnings)
 {
-	Stock_Info I;
-	I.Ticker = symbol;
-	I.Name = name_with_quotes;
-	I.Price = price;
-	I.Earnings = earnings;
+	Update_Ticker(symbol);
+	Update_Name(name_with_quotes);
+	Update_Price(price);
+	Update_Earnings(earnings);
 
-	std::cout << I.Ticker << I.Name << I.Price << I.Earnings << std::endl;
+	cout << left << setw(5) << symbol << setw(27) << name_with_quotes << "Price: " << setw(10) << price
+		 << "Earnings " << setw(7) << earnings << endl;
+
+	//cout << left << setw(5) << Stock.Ticker << setw(27) << Stock.Name << "Price: " 
+		 //<< setw(10) << Stock.Price << "Earnings " << setw(7) << Stock.Earnings << endl;
 }
--- CST126_2/stock.h
+++ CST126_2/stock.h
@@ -1,14 +1,34 @@
-//--------------------------------------------------------------------
-// stock.h
-//     
-//--------------------------------------------------------------------
 #include <string>
+using namespace std;
 
-struct Stock_Info
+class Stock_Info
 {
-	std::string Ticker;
-	std::string Name;
-	double Price;
-	double Earnings;
-	double PE_Ratio;
+	public:
+		Stock_Info(string & symbol, string & name_with_quotes, double & price, double & earnings);
+		string Update_Ticker (string T) {
+			Ticker = T;
+			return T;
+		}
+		string Update_Name(string N) {
+			Name = N;
+			return N;
+		}
+		double Update_Price(double P) {
+			Price = P;
+			return P;
+		}
+		double Update_Earnings(double E) {
+			Earnings = E;
+			return E;
+		}
+		double Update_PE_Ratio(double PE) {
+			PE_Ratio = PE;
+			return PE;
+		}
+	private:
+		string Ticker;
+		string Name;
+		double Price;
+		double Earnings;
+		double PE_Ratio;
 };
