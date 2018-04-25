0b187f21ccd2fad4fb039a0ed5cef4775c0f4f17
SimiF <SimiF@users.noreply.github.com>
2018-04-25 16:12:03 -0700

    These are updated. Functional but not perfect style - wise.

--- CST126_2/stock.cpp
+++ CST126_2/stock.cpp
@@ -15,9 +15,23 @@ Stock_Info::Stock_Info(string & symbol, string & name_with_quotes, double & pric
 	Update_Price(price);
 	Update_Earnings(earnings);
 
-	cout << left << setw(5) << symbol << setw(27) << name_with_quotes << "Price: " << setw(10) << price
-		 << "Earnings " << setw(7) << earnings << endl;
+	//cout << left << setw(5) << Return_Ticker() << setw(27) << Return_Name() << "Price: " << setw(10) 
+		// << Return_Price() << "Earnings " << setw(7) << Return_Earnings() << "P/E: "
+		 //<< setw(5) << Calculate_PE() << endl;
 
 	//cout << left << setw(5) << Stock.Ticker << setw(27) << Stock.Name << "Price: " 
 		 //<< setw(10) << Stock.Price << "Earnings " << setw(7) << Stock.Earnings << endl;
 }
+
+
+/*int i = 0;
+stock_array[i].Update_Ticker(symbol);
+stock_array[i].Update_Name(name_with_quotes);
+stock_array[i].Update_Price(price);
+stock_array[i].Update_Earnings(earnings);
+
+cout << left << setw(5) << stock_array[i].Return_Ticker() << setw(27) << stock_array[i].Return_Name()
+<< "Price: " << setw(10) << stock_array[i].Return_Price() << "Earnings " << setw(7)
+<< stock_array[i].Return_Earnings() << "P/E: " << setw(5) << stock_array[i].Calculate_PE(price, earnings)
+<< endl;
+i++;*/
--- CST126_2/stock.h
+++ CST126_2/stock.h
@@ -4,31 +4,70 @@ using namespace std;
 class Stock_Info
 {
 	public:
+		Stock_Info() { Ticker = '\0'; Name = '\0'; Price = 0; Earnings = 0; };
 		Stock_Info(string & symbol, string & name_with_quotes, double & price, double & earnings);
-		string Update_Ticker (string T) {
+		void Stock_Update(string & symbol, string & name_with_quotes, double & price, double & earnings)
+		{
+			Ticker = symbol;
+			Name = name_with_quotes;
+			Price = price;
+			Earnings = earnings;
+		}
+		string Update_Ticker(string T) 
+		{
 			Ticker = T;
 			return T;
 		}
-		string Update_Name(string N) {
+		string Update_Name(string N) 
+		{
 			Name = N;
 			return N;
 		}
-		double Update_Price(double P) {
+		double Update_Price(double P) 
+		{
 			Price = P;
 			return P;
+
 		}
-		double Update_Earnings(double E) {
+		double Update_Earnings(double E) 
+		{
 			Earnings = E;
 			return E;
 		}
-		double Update_PE_Ratio(double PE) {
-			PE_Ratio = PE;
-			return PE;
+		string Return_Ticker()
+		{
+			return Ticker;
+		}
+		string Return_Name()
+		{
+			return Name;
+		}
+		double Return_Price()
+		{
+			return Price;
+		}
+		double Return_Earnings()
+		{
+			return Earnings;
+		}
+		double Percent_Increase_Price(double percent)
+		{
+			double Increased_Price = (100 - percent) * Price;
+			return Increased_Price;
+		}
+		double Percent_Increase_Earnings(double percent)
+		{
+			double Increased_Earnings = (100 - percent) * Earnings;
+			return Increased_Earnings;
+		}
+		double Calculate_PE()
+		{
+			double PE_Ratio = Price / Earnings;
+			return PE_Ratio;
 		}
 	private:
 		string Ticker;
 		string Name;
 		double Price;
 		double Earnings;
-		double PE_Ratio;
 };
